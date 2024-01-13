dbb381eb7048e251762c172344e0e4cd8b47c24c
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-01-13 16:09:29 +0100

    Add frontend for hero section, user authentication with clerk

---
+++ constants/index.ts
@@ -0,0 +1,16 @@
+export const headerLinks = [
+	{
+		label: 'Home',
+		route: '/',
+	},
+	{
+		label: 'Create Event',
+		route: '/events/create',
+	},
+	{
+		label: 'My Profile',
+		route: '/profile',
+	},
+];
+
+export const eventDefaultVAlues = {};
