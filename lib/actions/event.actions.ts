2cf7f11711a7c579ae021179a9b08b8d2a0ca53e
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-04-02 17:26:31 +0200

    add single event detail page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -2,10 +2,20 @@
 
 import { connectToDatabase } from '../database';
 import { CreateEventParams } from '@/types';
+import { handleError } from '../utils';
+import User from '../database/models/user.model';
 import Event from '../database/models/event.model';
 import Category from '../database/models/category.model';
-import User from '../database/models/user.model';
-import { handleError } from '../utils';
+
+const populateEvent = async (query: any) => {
+	return query
+		.populate({
+			path: 'organizer',
+			model: User,
+			select: '_id firstName lastName',
+		})
+		.populate({ path: 'category', model: Category, select: '_id name' });
+};
 
 export const createEvent = async ({
 	userId,
@@ -28,6 +38,23 @@ export const createEvent = async ({
 		});
 
 		return JSON.parse(JSON.stringify(newEvent));
+	} catch (error) {
+		handleError(error);
+	}
+};
+
+export const getEventById = async (eventId: string) => {
+	try {
+		await connectToDatabase();
+
+		// get the event
+		const event = await populateEvent(Event.findById(eventId));
+
+		if (!event) {
+			throw new Error('Event not found');
+		}
+
+		return JSON.parse(JSON.stringify(event));
 	} catch (error) {
 		handleError(error);
 	}
