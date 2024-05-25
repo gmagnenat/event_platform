c98acea36828ab252d2c37ee3c5f2a8dccbd8c14
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-05-25 23:04:57 +0200

    Add actions and form to update events

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,25 +1,38 @@
-import EventForm from '@/components/shared/EventForm';
-import { auth } from '@clerk/nextjs';
-import React from 'react';
+import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/event.actions";
+import { auth } from "@clerk/nextjs";
+import React from "react";
 
-const UpdateEvent = () => {
-	const { sessionClaims } = auth();
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
 
-	const userId = sessionClaims?.userId as string;
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
 
-	return (
-		<>
-			<section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
-				<h3 className='wrapper h3-bold text-center sm:text-left'>
-					Update an event
-				</h3>
-			</section>
+  const event = await getEventById(id);
 
-			<div className='wrapper my-8'>
-				<EventForm userId={userId} type='Update' />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Update an event
+        </h3>
+      </section>
+
+      <div className="wrapper my-8">
+        <EventForm
+          userId={userId}
+          type="Update"
+          event={event}
+          eventId={event._id}
+        />
+      </div>
+    </>
+  );
 };
 
 export default UpdateEvent;
